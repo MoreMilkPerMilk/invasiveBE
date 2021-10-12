c14af9569cab7e914d220114f635948c831a2455
Djamahl <djamahletchegaray@gmail.com>
2021-10-12 10:52:04 +1000

    change to port 80

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ app.include_router(species.router)
 app.mount("/files", StaticFiles(directory="files"), name="files")
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("app:app", host='0.0.0.0', port=80, reload=True)
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-uvicorn app:app --host 0.0.0.0 --port 8080 --reload
+uvicorn app:app --host 0.0.0.0 --port 80 --reload
