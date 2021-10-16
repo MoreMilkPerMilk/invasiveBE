74c70ee014644297a14b5f58075b2e8b9594d9cc
Djamahl <djamahletchegaray@gmail.com>
2021-10-16 16:15:59 +1000

    species collection population

--- routers/species.py
+++ routers/species.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Request, HTTPException
 from typing import List
 
+import requests
+
 from Models.Species import Species
 
 import routers.photolocations
@@ -62,3 +64,33 @@ def species_search(request: Request, species_name: str = ""):
         raise HTTPException(status_code=404)
 
     return [Species(**s) for s in res]
+
+@router.put("/populate")
+def populate(request: Request):
+    print("populating species")
+    WEED_URL = "https://weeds.brisbane.qld.gov.au/api/weeds"
+    species_collection = request.app.state.db.data.species
+    
+    r = requests.get(WEED_URL)
+    print(r.json())
+    entries = []
+    for s in r.json():
+        entry = {
+            "species_id": s["Nid"],
+            "name": s["Name"],
+            "species": s['Species name'],
+            # "info":
+            "family": s["Family"],
+            "native": True if s["Native/Exotic"] == "Native" else False,  # thx aussie gov't
+            "common_names": s["Common names"],
+            "notifiable": True if s["Notifiable"] == "Yes" else False,
+            "growth_form": s["Growth form"],
+            "flower_colour": s["Flower colour"],
+            "leaf_arrangement": s["Leaf arrangement"],
+            "flowering_time": s["Flowering time"],
+            "state_declaration": s["State declaration"],
+            "council_declaration": s["Council declaration"],
+            "control_methods": s["Control methods"],
+            "replacement_species": s["Replacement species"],
+        }
+        species_collection.insert_one(Species(**entry).dict())
